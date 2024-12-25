acc396a07261f1d312c9aa576e788e37e89d3c5e
Surinderpro007 <singhsurinder4565@gmail.com>
2024-12-26 00:13:10 +0530

    Your commit message here

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
+const User = require('../models/User');
 
-router.get("/",(req,res)=>{
-    obj={
-        "name":"John",
-        "age":30,
+
+router.post("/", [
+  body('name').notEmpty().withMessage('Name is required'), 
+  body('email', 'Enter a valid email').isEmail(),         
+  body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters long')
+], async(req, res) => {
+  const result = validationResult(req);
+
+  if (!result.isEmpty()) {
+    return res.status(400).json({ errors: result.array() });
+  }
+  try{
+
+     const user = await User.create({
+          name : req.body.name,
+          email : req.body.email,
+          password : req.body.password
+        })
+        return res.status(201).json(user)
+    } 
+    catch(error){
+            return res.status(500).json({message : "Internal Server Error"})
     }
-    res.json(obj);
-})
+
+//   res.send(req.body); 
+});
 
 module.exports = router;
